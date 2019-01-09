be60c137556ffb2d2517d705333c6c7ee6606cbd
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2019-01-09 12:15:59 -0200

    Adicionando triggers para animação do herói.

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour
+{
+    private MovimentacaoPersonagem movimentacaoPersonagem;
+    private Animator characterAC;
+
+    // Use this for initialization
+    void Start()
+    {
+        movimentacaoPersonagem = GetComponent<MovimentacaoPersonagem>();
+        characterAC = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateAnimations();
+    }
+
+    public void UpdateAnimations()
+    {
+        characterAC.SetBool("Walk", movimentacaoPersonagem.GetIsWalking());
+        print(characterAC.GetBool("Walk") + " - " + Time.deltaTime);
+    }
+}
