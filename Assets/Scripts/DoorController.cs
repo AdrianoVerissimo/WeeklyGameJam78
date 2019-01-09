a61dcf276072ccbc785221da36887c1d9a6ad075
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2019-01-09 11:59:29 -0200

    Fazendo porta abrir/fechar.

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : MonoBehaviour
+{
+    public bool isOpen = false;
+
+    private Animator doorAC;
+
+    // Use this for initialization
+    void Start()
+    {
+        doorAC = GetComponent<Animator>();
+
+        if (isOpen)
+            Open(false);
+        else
+            Close(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isOpen)
+            {
+                Close(true);
+            }
+            else
+            {
+                Open(true);
+            }
+        }
+    }
+
+    public void Open(bool useAnimation)
+    {
+        doorAC.SetBool("On", true);
+        doorAC.SetBool("UseAnimation", useAnimation);
+
+        isOpen = true;
+    }
+    public void Close(bool useAnimation)
+    {
+        doorAC.SetBool("On", false);
+        doorAC.SetBool("UseAnimation", useAnimation);
+
+        isOpen = false;
+    }
+}
