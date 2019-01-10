a544898bb23cd95fbdba7818deae356d1a54bbf4
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2019-01-10 13:15:21 -0200

    Criando prefab para fim de fase.

---
+++ Assets/Scripts/EndStage.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndStage : MonoBehaviour
+{
+    public string sceneGoTo = "";
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GoToSceneController.GoToScene(sceneGoTo);
+        }
+    }
+}
