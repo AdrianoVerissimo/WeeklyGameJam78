1eae0773456952276695d467b94de911b53d021f
Fábio Lamin <fabio_lamin@hotmail.com>
2019-01-28 19:46:23 -0200

    Implementando a condição para inicar Fade Out e a próxima cena

--- Assets/Scripts/EndStage.cs
+++ Assets/Scripts/EndStage.cs
@@ -1,27 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class EndStage : MonoBehaviour
 {
-    public string sceneGoTo = "";
+    //public string sceneGoTo = "";
+    public Animator fadeOut; // Animator Controller do Fade Out
+    public string nextSceneName; // Nome da próxima cena que será carregada
+    private float waiting = 2f; // Tempo de espera necessário para que a cena comece a carregar
+    private bool waitForNextScene = false; // Condição para começar a contagem
 
-    // Use this for initialization
-    void Start()
-    {
 
+    void Update() {
+        if(waitForNextScene) //Caso verdadeiro 
+        {
+            waiting -= Time.deltaTime; // Iniciar contagem
+
+            if(waiting <= 0 ) // Caso contagem menor ou igual a 0 
+            {
+                SceneManager.LoadScene(nextSceneName); // Carregar próxima cena de acordo com o nome passado como parâmetro
+                waiting = 2f; // Resetar a contagem
+            }
+        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player")) //Quando o EndStage entrar em contato com o Player
         {
-            GoToSceneController.GoToScene(sceneGoTo);
+            fadeOut.SetTrigger("SetFadeOut"); // Iniciar a animação de Fade Out
+            waitForNextScene = true; // Validar como verdadeiro a contagem
+            
         }
     }
 }
